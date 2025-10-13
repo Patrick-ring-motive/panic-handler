3c8b964b55e1706b1bd05ff06fa2a8bbf8c13bf3
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-10-12 20:47:42 -0500

    Update main.go

--- main.go
+++ main.go
@@ -3,59 +3,18 @@ package main
 import (
   "log"
   "fmt"
-  "runtime/debug"
+  "github.com/Patrick-ring-motive/panic-handler/panics"
 )
 
-func init(){
-  debug.SetPanicOnFault(true)
-}
-
-type Panic struct{
-  Recover any
-  Message string
-  Stack string
-}
-
-func (p Panic)Error() string{
-  return p.Message
-}
-
-type PanicHandler struct{
-  Try func()
-  Catch func(Panic)
-  Finally func()
-}
-
-func (p PanicHandler)Handle(){
-  if p.Try == nil{
-    panic("Try should not be nil")
-  }
-  debug.SetPanicOnFault(true)
-  if p.Finally != nil{
-    defer p.Finally()
-  }
-  if p.Catch != nil{
-    defer func(){
-      if r := recover(); r != nil{
-        p.Catch(Panic{
-          Recover: r,
-          Message: fmt.Sprintf("Panic recovered: %T %+v",r,r),
-          Stack: string(debug.Stack()),
-        })
-      }
-    }()
-  }
-  p.Try()
-}
 
 func main() {
 
-  PanicHandler{
+  panics.PanicHandler{
     Try: func(){
       log.Println("Try\n")
       panic(fmt.Errorf("Thrown Error"))
     },
-    Catch: func(p Panic){
+    Catch: func(p panics.Panic){
       log.Printf("Catch\n%+v\n",p.Message)
     },
     Finally: func(){
